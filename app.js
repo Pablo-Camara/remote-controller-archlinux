8effc1f19ae87c4d9b3f1ca0f92bf986ea186047
Pablo Camara <pablo.camara.pt@gmail.com>
2021-01-16 22:05:10 +0000

    more features

--- app.js
+++ app.js
@@ -36,58 +36,98 @@ app.get('/',function(req,res) {
 
 app.get('/mute', function(req, res){
     cli('pactl set-sink-mute @DEFAULT_SINK@ toggle');
+
+    res.sendStatus(200).send();
 });
 
 app.get('/volume/:action/:v', function(req, res){
     cli('pactl set-sink-volume @DEFAULT_SINK@ ' + (req.params.action === 'increase' ? '+' : '-') + req.params.v + '%');
+
+    res.sendStatus(200).send();
 });
 
 app.get('/mouse/click/left', function(req, res){
     cli('xdotool click 1');
+    
+    res.sendStatus(200).send();
 });
 
 app.get('/mouse/click/middle', function(req, res){
     cli('xdotool click 2');
+
+    res.sendStatus(200).send();
 });
 
 app.get('/mouse/click/right', function(req, res){
     cli('xdotool click 3');
+
+    res.sendStatus(200).send();
 });
 
 app.get('/mouse/scroll/up', function(req, res){
     cli('xdotool click 4');
+
+    res.sendStatus(200).send();
 });
 
 app.get('/mouse/scroll/down', function(req, res){
     cli('xdotool click 5');
+
+    res.sendStatus(200).send();
 });
 
 app.get('/mouse/x/:x/y/:y', function(req, res){
     cli('xdotool mousemove ' + req.params.x + ' ' + req.params.y);
+
+    res.sendStatus(200).send();
 });
 
 app.get('/alt/down', function(req, res){
     cli('xdotool keydown alt');
+
+    res.sendStatus(200).send();
 });
 
 app.get('/alt/up', function(req, res){
     cli('xdotool keyup alt');
+
+    res.sendStatus(200).send();
+});
+
+app.get('/ctrl/down', function(req, res){
+    cli('xdotool keydown ctrl');
+
+    res.sendStatus(200).send();
+});
+
+app.get('/ctrl/up', function(req, res){
+    cli('xdotool keyup ctrl');
+
+    res.sendStatus(200).send();
 });
 
 app.get('/tab', function(req, res){
     cli('xdotool key Tab');
+
+    res.sendStatus(200).send();
 });
 
 app.get('/key/:key', function(req, res){
     cli('xdotool key ' + req.params.key);
+
+    res.sendStatus(200).send();
 });
 
-app.get('/cli/:cmd', function(req, res){
-    cli(req.params.cmd);
+app.get('/cli', function(req, res){
+    cli(req.query.cmd);
+
+    res.sendStatus(200).send();
 });
 
 app.get('/kill/active-window', function(req, res){
     cli('xdotool getactivewindow | xargs xdotool windowkill');
+
+    res.sendStatus(200).send();
 });
 
 app.listen(port, hostname, () => {
