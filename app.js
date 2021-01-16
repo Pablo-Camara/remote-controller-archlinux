c1d0fa2bcb0585093da4dc84a0b9e415ab7e86bf
Pablo Camara <pablo.camara.pt@gmail.com>
2021-01-16 21:21:18 +0000

    more features

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const { exec } = require("child_process");
+
 var cors = require('cors')
 
 var path = require('path');
@@ -9,10 +11,10 @@ const port = 3000;
 var express = require('express');
 var app = express();
 
+    const { exec } = require("child_process");
 app.use(cors());
 
 function cli(cmd){
-    const { exec } = require("child_process");
     console.log(`Executing: ${cmd}`)
     exec(cmd, (error, stdout, stderr) => {
         if (error) {
@@ -40,6 +42,49 @@ app.get('/volume/:action/:v', function(req, res){
     cli('pactl set-sink-volume @DEFAULT_SINK@ ' + (req.params.action === 'increase' ? '+' : '-') + req.params.v + '%');
 });
 
+app.get('/mouse/click/left', function(req, res){
+    cli('xdotool click 1');
+});
+
+app.get('/mouse/click/middle', function(req, res){
+    cli('xdotool click 2');
+});
+
+app.get('/mouse/click/right', function(req, res){
+    cli('xdotool click 3');
+});
+
+app.get('/mouse/scroll/up', function(req, res){
+    cli('xdotool click 4');
+});
+
+app.get('/mouse/scroll/down', function(req, res){
+    cli('xdotool click 5');
+});
+
+app.get('/mouse/x/:x/y/:y', function(req, res){
+    cli('xdotool mousemove ' + req.params.x + ' ' + req.params.y);
+});
+
+app.get('/alt/down', function(req, res){
+    cli('xdotool keydown alt');
+});
+
+app.get('/alt/up', function(req, res){
+    cli('xdotool keyup alt');
+});
+
+app.get('/tab', function(req, res){
+    cli('xdotool key Tab');
+});
+
+app.get('/key/:key', function(req, res){
+    cli('xdotool key ' + req.params.key);
+});
+
+app.get('/cli/:cmd', function(req, res){
+    cli(req.params.cmd);
+});
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
