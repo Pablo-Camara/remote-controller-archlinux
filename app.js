a83fd3e26d9dd286b4a0e6bcfa01355f5ccf32d8
Pablo Camara <pablo.camara.pt@gmail.com>
2021-01-16 19:14:25 +0000

    initial actions / controls

---
+++ app.js
@@ -0,0 +1,46 @@
+const http = require('http');
+var cors = require('cors')
+
+var path = require('path');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+var express = require('express');
+var app = express();
+
+app.use(cors());
+
+function cli(cmd){
+    const { exec } = require("child_process");
+    console.log(`Executing: ${cmd}`)
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
+
+//assuming app is express Object.
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/mute', function(req, res){
+    cli('pactl set-sink-mute 0 toggle');
+});
+
+app.get('/volume/:action/:v', function(req, res){
+    cli('pactl set-sink-volume @DEFAULT_SINK@ ' + (req.params.action === 'increase' ? '+' : '-') + req.params.v + '%');
+});
+
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
